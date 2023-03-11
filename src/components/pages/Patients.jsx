066fa4612cadfc62ded56e8006d45f4d44b3471c
David Roque <davidroque21@hotmail.com>
2023-03-11 10:41:34 -0400

    changes made in patients

--- src/components/pages/Patients.jsx
+++ src/components/pages/Patients.jsx
@@ -1,52 +1,68 @@
-import { Box, Button } from '@mui/material'
+import { Box, Button, Modal } from '@mui/material'
 import { DataGrid } from '@mui/x-data-grid'
 import React from 'react'
 import { useGlobalContext } from '../../context/GlobalContext'
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: "100%",
+  maxWidth: 600,
+  bgcolor: 'background.paper',
+  border: '2px solid background.paper',
+  borderRadius: 4,
+  boxShadow: 24,
+  outline: 'none',
+  p: 4,
+};
+
+
+
 export const Patients = () => {
+  const [open, setOpen] = React.useState(false);
   const { patients } = useGlobalContext()
-  const patientColumns = [
-    {
-      header: 'Nombre',
-      accessor: 'firstname'
-    },
-    {
-      header: 'Apellido',
-      accessor: 'lastname'
-    },
-    {
-      header: 'Edad',
-      accessor: 'birthdate'
-    },
-    {
-      header: 'Sexo',
-      accessor: 'gender'
-    },
-    {
-      header: 'Teléfono',
-      accessor: 'telephone'
-    },
-  ]
 
   const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstname', headerName: 'First name', width: 100 },
-    { field: 'lastname', headerName: 'Last name', width: 120 },
+    { field: 'firstname', headerName: 'Nombres', width: 100 },
+    { field: 'lastname', headerName: 'Apellidos', width: 120 },
+    { field: 'fullname', headerName: 'Nombre completo', width: 200, valueGetter: (params) => `${params.row['firstname'] || ''} ${params.row['lastname'] || ''}` },
     { field: 'birthdate', headerName: 'Birthdate', width: 100 },
     { field: 'gender', headerName: "Sexo", width: 100 },
     { field: 'telephone', headerName: 'Teléfono', width: 120 },
   ]
 
+  const handleClose = () => setOpen(false);
+  const handleOpen = () => setOpen(true);
+
   return (
     <Box>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} >
+          fORM aQUI
+        </Box>
+      </Modal>
       <Box sx={{
         display: 'flex',
         justifyContent: 'flex-end',
         mb: '12px',
       }}>
-        <Button variant='contained' color='primary' sx={{
-          marginLeft: '10px',
-        }}>Crear</Button>
+        <Button
+          variant='contained'
+          color='primary' sx={{
+            marginLeft: '10px',
+          }}
+          onClick={handleOpen}
+        >
+          Crear
+        </Button>
       </Box>
       <div style={{ height: 400, width: '100%' }}>
         <DataGrid
@@ -54,7 +70,6 @@ export const Patients = () => {
           columns={columns}
           pageSize={5}
           rowsPerPageOptions={[5]}
-          checkboxSelection
         />
       </div>
     </Box>
